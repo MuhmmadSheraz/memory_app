2d05db96fa71e267bffbc108880299b58b75250a
MuhmmadSheraz <mshazy999@gmail.com>
2022-01-21 10:00:26 +0500

    add sort method for learning purpose

--- memory-backend/controller/Memory/GetMemories.ts
+++ memory-backend/controller/Memory/GetMemories.ts
@@ -1,50 +1,55 @@
 import { memories } from './../../model/memory';
-import { Request, Response } from "express"
-export const getAllMemories = async (req: Request, res: Response,) => {
-    let data;
-    try {
-        // finds all the docs.
-        //  data=await memories.find() 
+import e, { Request, Response } from 'express';
+export const getAllMemories = async (req: Request, res: Response) => {
+  let data;
+  try {
+    // finds all the docs.
+    //  data=await memories.find()
 
-        // Filter Methods
+    // Filter Methods
 
-        // ---- 1
-        // data = await memories.find({
-        //     title: req.query.title
-        //     // can add rest of the filter options
-        // })
+    // ---- 1
+    // data = await memories.find({
+    //     title: req.query.title
+    //     // can add rest of the filter options
+    // })
 
-        // ----2
-        // This is an shorthand method 1
-        // data = await memories.find(req.query)
+    // ----2
+    // This is an shorthand method 1
+    // data = await memories.find(req.query)
 
-        // ---3 mongooose method
-        // data=await memories.find().where("title").equals("Good Memory")
+    // ---3 mongooose method
+    // data=await memories.find().where("title").equals("Good Memory")
 
-// ************** Exculde Query
-            // const queryObj={...req.query} //Shallow copy of req.query
-            // const excludeFields=["sort","page","limit"] //  these are the query fields which I don't want to add in request
-            // excludeFields.forEach((exQ:string)=>delete queryObj[exQ])
-            // console.log(queryObj)
-            // data=await memories.find(queryObj)
+    // ************** Exculde Query
+    // const queryObj={...req.query} //Shallow copy of req.query
+    // const excludeFields=["sort","page","limit"] //  these are the query fields which I don't want to add in request
+    // excludeFields.forEach((exQ:string)=>delete queryObj[exQ])
+    // console.log(queryObj)
+    // data=await memories.find(queryObj)
 
-// ************** Advance Filter (Greater than or Less than)
-// Find Memory having morethan 5 likes
-let queryObj=JSON.stringify({...req.query})
-queryObj=queryObj.replace(/\b(gte|gt)\b/g ,match=> `$${match}`) //gte=greaterthanequalto || gt=greaterthan
-console.log(queryObj)
-data=await memories.find(JSON.parse(queryObj))
-        res.send({
-            status: 'success',
-            length: data?.length,
-            data
-        });
-    } catch (error: any) {
-        res.send({
-            status: 500,
-            message: error?.message
-        })
+    // ************** Advance Filter (Greater than or Less than)
+    // Find Memory having morethan 5 likes
+    // let queryObj=JSON.stringify({...req.query})
+    // queryObj=queryObj.replace(/\b(gte|gt)\b/g ,match=> `$${match}`) //gte=greaterthanequalto || gt=greaterthan
+    // console.log(queryObj)
+    // data=await memories.find(JSON.parse(queryObj))
+
+    // ************** Sort By Date
+    if (req.query.sort) {
+      data = await memories.find().sort(req.query.sort);
+    } else {
+      data = await memories.find();
     }
-
-
-}
+    res.send({
+      status: 'success',
+      length: data?.length,
+      data,
+    });
+  } catch (error:any) {
+    res.send({
+      status: 500,
+      message: error?.message,
+    });
+  }
+};
