41e5eee9533ccb10c1be749f12a14462fd373cb2
MuhmmadSheraz <mshazy999@gmail.com>
2022-01-23 14:09:08 +0500

    hash password and confirm password validation

--- memory-backend/model/user.ts
+++ memory-backend/model/user.ts
@@ -1,34 +1,52 @@
 import mongoose, { Schema } from "mongoose";
  import validator from "validator"
+ import bcrypt from "bcrypt"
 const userSchema:Schema= new Schema({
     name:{
         type:String,
-        require:[true,"name is required"],
+        required:[true,"name is required"],
         unique:true
         
     },
     email:{
         type:String,
-        require:[true,"email is required"],
+        required:[true,"email is required"],
         unique:true,
         validate:validator.isEmail
 
     },
     profilePicture:{
         type:String,
-        require:[true,"profilePicture is required"]
+        required:[true,"profilePicture is required"]
     },
     password:{
         type:String,
-        require:[true,"password is required"],
-        min:[6,"Password length in correct"]
+        required:[true,"password is required"],
+        minlength:6
 
     },
     confirmPassword:{
         type:String,
-        require:[true,"confirmPassword is required"],
-        min:[6,"Password length in correct"]
+        required:[true,"confirmPassword is required"],
+        minlength:6,
+        
 
     },
+})
+// Middleware for password confirmation
+userSchema.pre('validate', function(next) {
+    if (this.confirmPassword&&(this.password !== this.confirmPassword)) {
+        this.invalidate('passwordConfirmation', 'enter the same password');
+    }
+    next();
+});
+
+// Password hashing using middleware
+userSchema.pre("save",async function(next){
+    // do not has hashed the password if it's not modified or newly created
+    if(!this.isModified("password"))return next()
+    this.password=await bcrypt.hash(this.password,10)
+    this.confirmPassword=undefined
+    next()
 })
 export const userModal=mongoose.model("users",userSchema)
