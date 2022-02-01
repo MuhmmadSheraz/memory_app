e0bc69d2ab650e98f3ed048e548874a0f5996262
MuhmmadSheraz <mshazy999@gmail.com>
2022-02-01 11:44:02 +0500

    send cookie on signIn and signUp

--- memory-backend/controller/Authentication/auth.ts
+++ memory-backend/controller/Authentication/auth.ts
@@ -1,6 +1,6 @@
 import { sendEmail } from './../../helper/email';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import crypto from "crypto"
+import crypto from 'crypto';
 import { generateError } from './../../helper/generateError';
 import { NextFunction, Request, Response } from 'express';
 import { User, userModal } from './../../model/user';
@@ -15,6 +15,12 @@ export const signUp = async (req: Request, res: Response) => {
       confirmPassword: req.body.confirmPassword
     });
     const token = generateToken(newUser._id);
+    const expiry  = process.env.JWT_COOKIE_EXPIRY as string
+    // Sending Token In Cookie
+    res.cookie('jwt', token, {
+      expires: new Date((Date.now() + parseInt(expiry)) * 24 * 60 * 60 * 1000),
+      httpOnly: true
+    });
     res.send({
       status: 200,
       token,
@@ -26,8 +32,8 @@ export const signUp = async (req: Request, res: Response) => {
   }
 };
 export const signIn = async (req: Request, res: Response) => {
-  console.log("Sign In")
-  const { email, password } = req?.body;
+  console.log('Sign In');
+  const { email, password } = req.body;
   if (!email || !password)
     return generateError(res, 500, 'email and password are required');
   const user: User | any = await userModal
@@ -40,6 +46,11 @@ export const signIn = async (req: Request, res: Response) => {
   );
   if (!correctPassword) return generateError(res, 500, 'Incorrect password');
   const token = generateToken(user?._id);
+  const expiry  = process.env.JWT_COOKIE_EXPIRY as string
+  res.cookie('jwt', token, {
+    expires: new Date((Date.now() + parseInt(expiry)) * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  });
   res.send({
     status: 200,
     message: 'Signed In Successfully',
@@ -51,69 +62,71 @@ export const signIn = async (req: Request, res: Response) => {
 export const forgotPassword = async (req: Request, res: Response) => {
   // FD ---> Send Me Reset Email
 
-  console.log("Forgot API")
+  console.log('Forgot API');
   // 1) Find User By Email
   const user: User | any = await userModal.findOne({ email: req?.body?.email });
   if (!user) {
-     generateError(res, 404, 'No user found with this email');  
+    generateError(res, 404, 'No user found with this email');
   }
   // 2) Generate Password Expiry Token and save it to DB
-  const resetToken=await user?.createResetPasswordToken();
-  const message=`Your Password Reset Link is ---::----- ${resetToken}`
+  const resetToken = await user?.createResetPasswordToken();
+  const message = `Your Password Reset Link is ---::----- ${resetToken}`;
   await user.save();
-  sendEmail({email:user?.email,message})  
+  sendEmail({ email: user?.email, message });
   res.send({
-    status:200,
-    message:"Reset link has been sent to your email"
-  })
-  
+    status: 200,
+    message: 'Reset link has been sent to your email'
+  });
 };
-export const validateResetPasswordToken = async (req: Request, res: Response) => {
+export const validateResetPasswordToken = async (
+  req: Request,
+  res: Response
+) => {
   // For validating if token and expiry then only show green single to render {Reset Password Page}
-  let resetToken=req.params.token;
-  // hashing the token 
-  resetToken=crypto.createHash('sha256')
-  .update(resetToken)
-  .digest('hex'); 
-  
-  const user=await userModal.findOne({passwordResetToken:resetToken,expiryOfPasswordResetToken:{$gt:Date.now()}})
-  if(!user) generateError(res,404,"Invalid Token")
+  let resetToken = req.params.token;
+  // hashing the token
+  resetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+  const user = await userModal.findOne({
+    passwordResetToken: resetToken,
+    expiryOfPasswordResetToken: { $gt: Date.now() }
+  });
+  if (!user) generateError(res, 404, 'Invalid Token');
   res.send({
-    status:200,
-    message:"Request Successful"
-  })
-  
-}
+    status: 200,
+    message: 'Request Successful'
+  });
+};
 export const resetPassword = async (req: Request, res: Response) => {
   // For validating if token and expiry then only show green single to render {Reset Password Page}
-  let resetToken=req.params.token;
-  // hashing the token 
-  resetToken=crypto.createHash('sha256')
-  .update(resetToken)
-  .digest('hex'); 
-  
-try {
-  const user:any=await userModal.findOne({passwordResetToken:resetToken,expiryOfPasswordResetToken:{$gt:Date.now()}})
-  if(!user) generateError(res,404,"Invalid Token")
-   
-  // Finally updating the password
-  user.password=req?.body.password
-  user.passwordResetToken=undefined
-  user.expiryOfPasswordResetToken=undefined
-  user.save()
-  res.send({
-    status:201,
-    message:"Password has updated"
-  })
-} catch (error:any) {
-  generateError(res,500,error.message)
-}
-  
-}
+  let resetToken = req.params.token;
+  // hashing the token
+  resetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+  try {
+    const user: any = await userModal.findOne({
+      passwordResetToken: resetToken,
+      expiryOfPasswordResetToken: { $gt: Date.now() }
+    });
+    if (!user) generateError(res, 404, 'Invalid Token');
+
+    // Finally updating the password
+    user.password = req?.body.password;
+    user.passwordResetToken = undefined;
+    user.expiryOfPasswordResetToken = undefined;
+    user.save();
+    res.send({
+      status: 201,
+      message: 'Password has updated'
+    });
+  } catch (error: any) {
+    generateError(res, 500, error.message);
+  }
+};
 
 export const protect = async (
   req: Request,
-  res: Response,  
+  res: Response,
   next: NextFunction
 ) => {
   const headers: string = req?.headers.authorization || '';
